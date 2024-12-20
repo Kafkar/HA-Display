0e9ed52ccad9cc5f57e4fb8b19573f5480059566
niels26 <151180160+Kafkarcom@users.noreply.github.com>
2024-12-20 12:38:48 +0100

    addaed LVGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h> /
 
-// put function declarations here:
-int myFunction(int, int);
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+//Setup the serial port
+  Serial.begin(115200);
+
+//Setup the display
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+
+    // Print display sizes
+  Serial.println("Display sizes:");
+  Serial.print("Width: ");
+  Serial.println(tft.width());
+  Serial.print("Height: ");
+  Serial.println(tft.height());
+
+  // Display startup text
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(1);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("Kafkar HA-Display", tft.width() / 2, tft.height() / 2);
+
+  // Wait for 5 seconds
+  delay(5000);
+
+  // Clear the screen
+  tft.fillScreen(TFT_BLACK);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
